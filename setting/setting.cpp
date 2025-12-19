91e432ef43f224ec5ed8382c29dfe75d70e2ed1b
Long <dvhlong2535@apcs.fitus.edu.vn>
2025-12-19 15:35:19 +0700

    add edit wallet page

--- setting/setting.cpp
+++ setting/setting.cpp
@@ -47,11 +47,94 @@ void SettingPage(User& user) {
 }
 
 void EditUsernamePage(User& user) {
-    
+    bool t = true;
+    while(t){
+        ClearScreen();
+        cout << borderLine << '\n';
+        cout << "+------------------Edit Username Page----------------------+" << '\n';
+        cout << "[1] - Edit Name                                             " << '\n';
+        cout << "[2] - Back                                                  " << '\n';
+        int option = InputNumber("Option: ");
+        switch (option)
+        {
+        case 1:{
+            cout << "Current username: " << user.userName << '\n';
+            cout << "New Username: ";
+            cin.getline(user.userName, 51);
+            cout << "Create sucessful !" << '\n';
+            PauseScreen();
+            break;
+        }
+        case 2:{
+            t = 0;
+            break;
+        }
+        default:
+            cout << "Invalid input!" << '\n';
+            PauseScreen();
+            break;
+        }
+    }
 }
 
 void EditWalletPage(User& user) {
+    bool t = true;
+    while(t){
+        ClearScreen();
+        cout << borderLine << '\n';
+        cout << "+-------------------Edit Wallet Page-----------------------+" << '\n';
+        cout << "[1] - Show List Wallet                                      " << '\n';
+        cout << "[2] - Add Wallet                                            " << '\n';
+        cout << "[3] - Back                                                  " << '\n';
+        int option = InputNumber("Option: ");
+        switch (option)
+        {
+        case 1:{
+            bool t1 = true;
+            while(t1){
+                ClearScreen();
+                cout << borderLine << '\n';
+                // Hiển thị danh sách các loại ví 
+                cout << "+-------------------List Wallet Page-----------------------+" << '\n';
+                MyVector<Wallet> displayList;
+                for (int i=0; i<user.walletList.size; ++i){
+                    if (!user.walletList[i].isDeleted) cout << "[ " << i + 1 << " ] - " << user.walletList[i].name << '\n';
+                }
+                cout << "[0] - Back                                                  " << '\n';
 
+                cout << "Choose Wallet:                                              " << '\n';
+                int option = InputNumber("Option: ");
+                switch (option)
+                {
+                case 1:{
+                    t1 = 0;
+                    break;
+                }
+                case 2:{
+                    
+                }
+                default:
+                    cout << "Invalid input!" << '\n';
+                    PauseScreen();
+                    break;
+                }
+            }
+                    break;
+        }
+        case 2:{
+            
+            break;
+        }
+        case 3:{
+            t = 0;
+            break;
+        }
+        default:
+            cout << "Invalid input!" << '\n';
+            PauseScreen();
+            break;
+        }
+    }
 }
 
 void EditIncomeCategory(User& user) {
